889f8559e72ffd0476e2f4d9c65496f5bfb6e2fd
Yurika Jacobsson <nakiruy@gmail.com>
2017-10-09 21:40:47 -0700

    Add Save and New Task buttons.

---
+++ client/src/components/New.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class New extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+        <button type="button">New Task</button>
+      </div>
+    )
+  }
+}
+
+export default New
