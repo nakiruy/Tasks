69bc507becf72dbda1f529dc56998f1abdad61cc
Yurika Jacobsson <nakiruy@gmail.com>
2017-10-09 20:48:00 -0700

    Add axios to App component.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      tasks: [];
     };
+  }
+
+  componentDidMount() {
+  
   }
 
   render() {
     return (
       <div>
-        <h3>This is a React Boilerplate.</h3>
-        <h4>Feel free to add whatever you like :)</h4>
+        Tasks
       </div>
     )
   }
 }
 
-export default App;
+export default App
