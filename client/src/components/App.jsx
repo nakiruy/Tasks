0da0edd58380417f584e5236970d9e8b7840effa
Yurika Jacobsson <nakiruy@gmail.com>
2017-10-10 13:56:01 -0700

    Remove unnecessary component and moved it to the App component.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 
+import Save from './Save';
+import New from './New';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -21,10 +24,18 @@ class App extends React.Component {
       })
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+
+
+  }
+
   render() {
     return (
       <div>
-        Tasks
+        <h1>Tasks</h1>
+        <button type="button">Save</button>
+        <New />
         <li>{this.state.tasks.tasks}</li>
       </div>
     )
